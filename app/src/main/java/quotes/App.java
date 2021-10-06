600809de3a9b30aa4e08546cfafc8f6bc8a3450e
Kafaween <abod.kafaween@gmail.com>
2021-10-06 21:54:01 +0300

    done

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -5,13 +5,15 @@ package quotes;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 
 import javax.xml.transform.Result;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class App {
@@ -19,15 +21,37 @@ public class App {
     private static final Type REVIEW_TYPE = new TypeToken<List<qout>>() {
     }.getType();
     public static void main(String[] args) throws Exception {
+        try {
+            URL url = new URL("http://api.fapi/1.0/?method=getQuote&format=json&lang=en");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.setRequestMethod("GET");
+           BufferedReader reader=new BufferedReader((new InputStreamReader(connection.getInputStream())));
 
-        Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader("./app/src/main/resources/recentquotes.json"));
-        List<qout> data = gson.fromJson(reader, REVIEW_TYPE);
-        System.out.println(data.size());
-        int range = (137 - 0) + 1;
-        int x=(int)(Math.random() * range) + 0;
+            String qo= reader.readLine();
+            reader.close();
+            Gson gson = new Gson();
+            qout data = gson.fromJson(qo,qout.class);
+            System.out.println(data);
+
+            BufferedReader reader_file = new BufferedReader(new FileReader("./app/src/main/resources/recentquotes.json"));
+            List<qout> data_in = gson.fromJson(reader_file, REVIEW_TYPE);
+            data_in.add(data);
+            String jsonInString = gson.toJson(data_in);
+            BufferedWriter write = new BufferedWriter(new FileWriter("./app/src/main/resources/recentquotes.json" , false));
+            write.write(jsonInString);
+            write.flush();
+
+            write.close();
+        } catch (IOException e) {
+            Gson gson = new Gson();
+            BufferedReader reader = new BufferedReader(new FileReader("./app/src/main/resources/recentquotes.json"));
+            List<qout> data = gson.fromJson(reader, REVIEW_TYPE);
+            int range = (data.size()) + 1;
+            int x = (int) (Math.random() * range) ;
             System.out.println(data.get(x));
             System.out.println("-----------------------------------------------------------------------------");
         }
-
+    }
 }
--- app/src/main/java/quotes/qout.java
+++ app/src/main/java/quotes/qout.java
@@ -8,21 +8,30 @@ public class qout {
     private String author;
     private String likes;
     private String text;
+    private String quoteText;
+    private String quoteAuthor;
+private String senderName;
+private String senderLink;
+private String quoteLink;
 
-    public qout(ArrayList<String> tags, String author, String likes, String text) {
+    public qout(ArrayList<String> tags, String author, String likes, String text, String quoteText, String quoteAuthor, String senderName, String senderLink, String quoteLink) {
         this.tags = tags;
         this.author = author;
         this.likes = likes;
         this.text = text;
+        this.quoteText = quoteText;
+        this.quoteAuthor = quoteAuthor;
+        this.senderName = senderName;
+        this.senderLink = senderLink;
+        this.quoteLink = quoteLink;
     }
 
     @Override
     public String toString() {
-        return "qout{" +
-                "tags=" + tags +
-                ", author='" + author + '\'' +
-                ", likes='" + likes + '\'' +
-                ", text='" + text + '\'' +
-                '}';
+        if(author!=null){
+        return author+" : "+text;
+    }else{
+            return quoteAuthor+" : "+quoteText;
+        }
     }
 }
